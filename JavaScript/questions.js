f130db504377fbdf28094e38ffdabf8aad730970
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-07 11:50:56 -0500

    questions 1/2

---
+++ JavaScript/questions.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Explain Hoisting in JavaScript.",
+    answer: "Hoisting is a default behavior of JavaScript where all the varaible and function declarations are moved to the top.",
+    options: [
+      "Hoisting is a default behavior of JavaScript where only the semantic tags and classes are moved to the bottom.",
+      "Hoisting is a default behavior of JavaScript where all the varaible and function declarations are moved to the bottom.",
+      "Hoisting is a default behavior of JavaScript where all the varaible and function declarations are moved to the top.",
+      "Hoisting is a default behavior of JavaScript where certain varaible and function declarations are linked together."
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is DOM",
+    answer: "The Document Object Model (DOM) is the data representation of the objects that comprise the structure and content of a document on the web.",
+    options: [
+      "The Document Object Model (DOM) is the data representation of the objects that comprise the structure and content of a document on the web.",
+      "The Data Object Markup (DOM) is the data representation of the objects that can be illustrated to coders during the testing stages of code.",
+      "The Document Object Mode (DOM) is the code that changes settings within the application to display data in other programming languages.",
+      "The Database Obstruction Model (DOM) will notify the user of potential coding erroes that may not be detected by generic coding programs and websites."
+    ]
+  },
+  {
+    numb: 3,
+    question: "Explain 'This' keyword.",
+    answer: "'This' refers to the object that the function is a property of.",
+    options: [
+      "'This' refers to the function that the object is a property of.",
+      "'This' refers to the object that the function is a property of.",
+      "'This' refers to the id that the semantic tag is a property of.",
+      "'This' refers to the entire code that the selected words and phrases are a property of."
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is NaN property in JavaScript?",
+    answer: "NaN Property represents 'Not-a-Number' value. It indicates a value which is not a legal number.",
+    options: [
+      "NaN Property represents 'Not-a-Normal' value. It indicates a value which is not in the selected programming language.",
+      "NaN Property represents 'Not-a-Null' value. It indicates a value which is not equal to zero.",
+      "NaN Property represents 'Not-a-Nuisance' value. It indicates a value which will not cause any problems in future code.",
+      "NaN Property represents 'Not-a-Number' value. It indicates a value which is not a legal number."
+    ]
+  }
+]
